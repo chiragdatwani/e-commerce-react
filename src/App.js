3b6e510f8dc7df8ac162f7afca238639d6524e6a
Chirag Datwani <chiragdatwani01@gmail.com>
2020-11-09 00:55:50 +0100

    finished sign in handleSubmit function

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
-        }, console.log(this.state));
+          console.log(this.state);
+        });
       } else {
         this.setState({ currentUser: userAuth });
       }
